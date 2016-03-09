2085f09ffd3e54ff3ee5cf5ab00dfbe3859865fc
nguyenxuanan <xuananitiu@gmail.com>
2016-03-09 08:40:53 +0700

    Độ Ẩm Đất
    Đọc được giá trị của sensor, độ nhay của sensor can được thử trong mội trường thực tế.

---
+++ SensorMonitor/Sensor_do_am_dat.ino
@@ -0,0 +1,22 @@
+/*
+
+Hien tai chi the hien duoc gia tri sensor: 
+
+So lieu thuc te can duoc test trong moi truong thich hop : vi du nh
+
+*/
+#define sensorPin A0
+int giatriAnalog, giatriDigital;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(sensorPin,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    giatriAnalog = analogRead(sensorPin);
+    Serial.println(" -----  Sensor value : -----");
+    Serial.println(giatriAnalog);
+    delay(1000);
+}
